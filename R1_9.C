8ee16d8196988927229d4ac212212683ffcd8d04
renuka-singare <57763433+renuka-singare@users.noreply.github.com>
2019-12-31 22:52:22 +0530

    Add files via upload

---
+++ R1_9.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char name[30],add[30];
+double phoneno;
+clrscr();
+printf("enter your name:");
+gets(name);
+printf("\n your name is:%s",name);
+printf("enter your add:");
+gets(add);
+printf("\n your add is:%s",add);
+print("enter your phoneno:");
+gets(phoneno);
+printf("\n your phoneno is:%s",phoneno);
+getch();
+}

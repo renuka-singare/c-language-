583d35346b0fe01e557dba2cfb853f172d431585
renuka-singare <57763433+renuka-singare@users.noreply.github.com>
2019-12-31 22:46:12 +0530

    Add files via upload

---
+++ R1_3.C
@@ -0,0 +1,19 @@
+	    #include<stdio.h>
+	    #include<conio.h>
+	    void main()
+	    {
+	    int a,b;
+	    int sum,diff,mul,div;
+	    clrscr();
+	    printf("enter two no.");
+	    scanf("%d%d",&a,&b);
+	    sum=a+b;
+	    diff=a-b;
+	    mul=a*b;
+	    div=a/b;
+	    printf("\n sum is %d",sum);
+	    printf("\n diff is %d",diff);
+	    printf("\n mul is %d",mul );
+	    printf("\n diff is %d",div);
+	    getch();
+	    }
